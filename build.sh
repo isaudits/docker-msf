c02a4417a5671de7502f81d0cbfd331c29981b87
Matt Jones <matt.jones@symphona.us>
2023-01-25 17:38:14 -0500

    update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-hooks/build
+#hooks/build
+#docker build -t isaudits/msf:base .
+docker build -t isaudits/msf:full -t isaudits/msf:latest -f Dockerfile.full .
+#docker build -t isaudits/msf:minimal -f Dockerfile.min .
 docker image prune -f
